96019884d120cd4a501a8ac6b61edaf7c3100e59
Anna Malanova <42782581+ane4ka0205@users.noreply.github.com>
2019-02-17 08:38:59 -0600

    Update myscript.groovy

--- vars/com/myscript.groovy
+++ vars/com/myscript.groovy
@@ -1,1 +1,18 @@
-echo "hello world"
+def exampleOfListFunction(item = []) {
+  if (item.size() > 0) {
+    item.each() {
+      println("print ${it}")
+    }
+  }
+}
+def myList = [0, 1, 1, 3, 4]
+// exampleOfListFunction(myList)
+
+
+def exampleOfDictionaryFunction(data=[:]) {
+  if (data) {
+    data.each() { key, value ->
+      println("User ${key} is ${value}")
+    }
+  }
+}
